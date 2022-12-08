6da8cce615c869f06067723cde77d0a97a7fbf03
Francisco Gonzalez <fagonzalh@gmail.com>
2022-12-07 23:42:12 -0500

    deployn1

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function initialize() {
 	var myOptions = {
 		zoom: 16,
-		center: new google.maps.LatLng(-2.1890375, -79.900023), //change the coordinates
+		center: new google.maps.LatLng(-2.2116245, -79.8892658), //change the coordinates
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		scrollwheel: false,
 		mapTypeControl: false,
@@ -11,7 +11,7 @@ function initialize() {
 	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 	var marker = new google.maps.Marker({
 		map: map,
-		position: new google.maps.LatLng(51.488966, -0.096777) //change the coordinates
+		position: new google.maps.LatLng(-2.2116245, -79.8892658) //change the coordinates
 	});
 	google.maps.event.addListener(marker, "click", function () {
 		infowindow.open(map, marker);
