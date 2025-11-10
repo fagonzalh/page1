faadb0f9c2856ce907cfcb87bcde0eb411160a97
Francisco Gonzalez <fagonzalh@gmail.com>
2025-11-10 08:39:58 -0500

    fix: cambios 9/11

--- js/map.js
+++ js/map.js
@@ -1,20 +1,15 @@
-function initialize() {
-	var myOptions = {
-		zoom: 16,
-		center: new google.maps.LatLng(-2.2116245, -79.8892658), //change the coordinates
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		scrollwheel: false,
-		mapTypeControl: false,
-		zoomControl: false,
-		streetViewControl: false
-	};
-	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-	var marker = new google.maps.Marker({
-		map: map,
-		position: new google.maps.LatLng(-2.2116245, -79.8892658) //change the coordinates
-	});
-	google.maps.event.addListener(marker, "click", function () {
-		infowindow.open(map, marker);
-	});
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+// Coordenadas del punto central
+const coordenadas = [-2.2116245, -79.8892658];
+
+// Inicializar el mapa
+const map = L.map('map').setView(coordenadas, 16);
+
+// Capa base: OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Agregar un marcador
+const marker = L.marker(coordenadas).addTo(map);
+marker.bindPopup("<b>ElectricTech</b><br>Guayaquil, Ecuador").openPopup();
